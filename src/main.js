5ad611b361b66f28244ba4ff2092a02078208154
KrawaUndefitied <krawa2011@gmail.com>
2025-03-24 20:41:03 +0600

    One Commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+/*Коментарии для бро, если воспользоваться горячими клавишами
+ctrl+/(однострочный комментарий)
+или ctrl+shift+/(многострочный коментарий), то в целом проще жить)))
+ */
+alert("Bro, do you want to see a new world?");
+alert("New World!");
+let admin, name;
+name="John";
+admin=name;
+alert(admin);
+let Earth, UserPage;
+Earth = 25;
